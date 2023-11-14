0c29baf840b2336d415bb321beabdbe84e8720cf
AER\fgalvan <fgalvan@aer.com.mx>
2023-11-14 15:21:12 -0600

    Pruebas 2.0 Peel Review Jairo

--- index.js
+++ index.js
@@ -1,52 +1,260 @@
+// Express routing module
 const express = require('express')
-const app = express()
-const path = require('path')
+const fileUpload = require("express-fileupload");
+
+// Pdf Reading module
+const pdfParse = require("pdf-parse");
+const bodyParser = require("body-parser");
+
+// Variable app
+const app = express();
+const path = require('path');
+
+// fs module
+const fs = require("fs");
 
 // Natural IA See Documentation: https://github.com/NaturalNode/natural
 const natural = require('natural')
 const classifier = new natural.BayesClassifier();
-
-console.log('Creando registros...');
-classifier.addDocument('Advertencia: Hubo una falla en el servidor 45', 'advertencia');
-classifier.addDocument('Error en la VPN 45', 'error');
-classifier.addDocument('El usuario root intentó loguear con error', 'error');
-classifier.addDocument('Se conectó un usuario a la red interna', 'informativo');
-classifier.addDocument('Precaución en el servidor 560, el uso de CPU está al 80%', 'advertencia');
-classifier.addDocument('Se detectó alta latencia en el servidor 893, revisar la conexión con fallas', 'advertencia');
-classifier.addDocument('Guardado el registro de la última hora de accesos a la VPN', 'informativo');
-classifier.addDocument('La máquina virtual 431 fue apagada exitosamente', 'informativo');
-classifier.addDocument('El usuario mi_empresa accedió por SSH exitosamente al servidor 33', 'informativo');
-classifier.addDocument('Error crítico en el servidor HTTP principal, cantidad de errores 500 excedido a lo permitido', 'error');
-classifier.addDocument('El servidor 15 presentó un error en el sistema operativo y se apagó inesperadamente.', 'error');
-
-console.log('Entrenando...');
-classifier.train();
-
-console.log('Realizando prueba de clasificación...');
-console.log(classifier.classify('La máquina virtual 510 tiene el uso de disco duro al 90%'));
-console.log(classifier.classify('El servidor 333 presentó un error en hardware terminando con BSOD'));
-
-console.log('Guardando clasificaciones...');
-classifier.save('clasificaciones.json');
-
-console.log('Cargando clasificaciones...');
-natural.BayesClassifier.load('clasificaciones.json', null, function(err, classifier) {
-  console.log(classifier.classify('El usuario 615 se desconectó del SSH correctamente'));
-  console.log(classifier.classify('La VPN 116 fue agregada con éxito'));
-  console.log(classifier.classify('Servidores del rack 15 con alta latencia'));
-  console.log(classifier.getClassifications('La máquina virtual 612 falló inesperadamente'));
-});
 
 // Settings
 app.set('port', 3000)
 
 // Middleware
 app.use(express.static(path.join(__dirname,'public')))
+app.engine('html', require('ejs').renderFile);
+app.use(fileUpload());
+app.use(bodyParser.urlencoded({extended: false}));
 
-// Routes
-app.get('/', (req, res)=>{
-    res.send('Welcome! :)')
-})
+console.log('Creando registros...');
+fnSamplesTrainingIA();
+
+// AUTOS SAMPLES
+async function fnSamplesTrainingIA(){
+/* fs.readFile(__dirname + "/src/documents/sample_auto_Banorte.txt", (err, data) => {
+    if (err){
+        console.log(err);
+    } else {
+        classifier.addDocument(data, 'Automóviles-Flotilla');
+    }
+});
+
+fs.readFile(__dirname + "/src/documents/sample_auto_Chubb.txt", "utf-8", (err, data) => {
+    if (err){
+        console.log(err);
+    } else {
+        classifier.addDocument(data, 'Automóviles-Flotilla');
+    }
+});
+
+fs.readFile(__dirname + "/src/documents/sample_auto_Gnp.txt", "utf-8", (err, data) => {
+    if (err){
+        console.log(err);
+    } else {
+        classifier.addDocument(data, 'Automóviles-Flotilla');
+    }
+});
+
+fs.readFile(__dirname + "/src/documents/sample_auto_Primero.txt", "utf-8", (err, data) => {
+    if (err){
+        console.log(err);
+    } else {
+        classifier.addDocument(data, 'Automóviles-Flotilla');
+    }
+});
+
+fs.readFile(__dirname + "/src/documents/sample_auto_Qualitas.txt", "utf-8", (err, data) => {
+    if (err){
+        console.log(err);
+    } else {
+        classifier.addDocument(data, 'Automóviles-Flotilla');
+    }
+});
+
+// DENTAL SAMPLES
+fs.readFile(__dirname + "/src/documents/sample_dental_Dentalia.txt", "utf-8", (err, data) => {
+    if (err){
+        console.log(err);
+    } else {
+        classifier.addDocument(data, 'Dental');
+    }
+});
+
+fs.readFile(__dirname + "/src/documents/sample_dental_Dentegra.txt", "utf-8", (err, data) => {
+    if (err){
+        console.log(err);
+    } else {
+        classifier.addDocument(data, 'Dental');
+    }
+});
+
+// GMM Samples
+fs.readFile(__dirname + "/src/documents/sample_gmm_Atlas.txt", "utf-8", (err, data) => {
+    if (err){
+        console.log(err);
+    } else {
+        classifier.addDocument(data, 'GMM');
+    }
+});
+
+fs.readFile(__dirname + "/src/documents/sample_gmm_Axxa.txt", "utf-8", (err, data) => {
+    if (err){
+        console.log(err);
+    } else {
+        classifier.addDocument(data, 'GMM');
+    }
+});
+
+fs.readFile(__dirname + "/src/documents/sample_gmm_Gnp.txt", "utf-8", (err, data) => {
+    if (err){
+        console.log(err);
+    } else {
+        classifier.addDocument(data, 'GMM');
+    }
+});
+
+fs.readFile(__dirname + "/src/documents/sample_gmm_Metlife.txt", "utf-8", (err, data) => {
+    if (err){
+        console.log(err);
+    } else {
+        classifier.addDocument(data, 'GMM');
+    }
+});
+
+// GMMenores Samples
+fs.readFile(__dirname + "/src/documents/sample_gmmen_MediAccess.txt", "utf-8", (err, data) => {
+    if (err){
+        console.log(err);
+    } else {
+        classifier.addDocument(data, 'GMMenores');
+    }
+});
+
+fs.readFile(__dirname + "/src/documents/sample_gmmen_Mms.txt", "utf-8", (err, data) => {
+    if (err){
+        console.log(err);
+    } else {
+        classifier.addDocument(data, 'GMMenores');
+    }
+});
+
+// Hogar Samples
+fs.readFile(__dirname + "/src/documents/sample_hogar_Chubb.txt", "utf-8", (err, data) => {
+    if (err){
+        console.log(err);
+    } else {
+        classifier.addDocument(data, 'Hogar');
+    }
+});
+
+fs.readFile(__dirname + "/src/documents/sample_hogar_Zurich.txt", "utf-8", (err, data) => {
+    if (err){
+        console.log(err);
+    } else {
+        classifier.addDocument(data, 'Hogar');
+    }
+});
+
+// Vida Samples
+fs.readFile(__dirname + "/src/documents/sample_vida_Gnp.txt", "utf-8", (err, data) => {
+    if (err){
+        console.log(err);
+    } else {
+        classifier.addDocument(data, 'Vida');
+    }
+}); */
+
+// Funerario Samples
+/* fs.readFile(__dirname + "/src/documents/sample_funerario_Thona.txt", "utf-8", (err, data) => {
+    if (err){
+        console.log(err);
+    } else {
+        console.log('No async');
+        classifier.addDocument(data, 'Funerario');
+    }
+});
+console.log("Read Sample files content ✅"); */
+
+    await prueba("/src/documents/sample_funerario_Thona.txt");
+    await fnTrainingDataIA();
+}
+
+async function prueba(ubicacion){
+    fs.readFile(__dirname + ubicacion, "utf-8", (err, data) => {
+        if (err){
+            console.log(err);
+            return err;
+        } else {
+            console.log('async prueba');
+            classifier.addDocument(data, 'Funerario');
+            return data;
+        }
+    });
+}
+
+function fnTrainingDataIA(){
+    console.log('Training IA...');
+    classifier.train();
+}
+
+
+
+/*
+console.log('Realizando prueba de clasificación...');
+fs.readFile(__dirname + "/src/documents/test_gmm_Gnp.txt", "utf-8", (err, data) => {
+    if (err){
+        console.log(err);
+    } else {
+        console.log( classifier.classify(data) );
+    }
+});
+
+fs.readFile(__dirname + "/src/documents/test_gmm_Axxa.txt", "utf-8", (err, data) => {
+    if (err){
+        console.log(err);
+    } else {
+        console.log( classifier.classify(data) );
+    }
+});
+
+console.log('Saving Classifications...');
+classifier.save('clasificaciones.json');
+
+console.log('Loading Classifications...');
+natural.BayesClassifier.load('clasificaciones.json', null, function(err, classifier) {
+    fs.readFile(__dirname + "/src/documents/test_gmm_Axxa2.txt", "utf-8", (err, data) => {
+        if (err){
+            console.log(err);
+        } else {
+            console.log(classifier.classify(data));
+        }
+    });
+
+    fs.readFile(__dirname + "/src/documents/test_gmm_Axxa3.txt", "utf-8", (err, data) => {
+        if (err){
+            console.log(err);
+        } else {
+            console.log(classifier.getClassifications(data));
+        }
+    });
+});
+*/
+app.post("/extract-text", (req, res) => {
+    if (!req.files && !req.files.pdfFile) {
+        res.status(400);
+        res.end();
+    }
+
+    pdfParse(req.files.pdfFile).then(result => {
+        res.send(result.text);
+    });
+});
+
+app.post("/classify-text", (req, res) => {
+    var texto = req.body.resultText;
+    console.log(texto);
+    res.render(__dirname + "/public/classify-text.html", {texto:texto});
+});
 
 app.listen(app.get('port'), ()=>{
     console.log('App listening in port ' + app.get('port'))
