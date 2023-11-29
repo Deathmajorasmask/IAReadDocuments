973b68917d02a36d1d7b19ee63589ba56d822f20
Deathmajorasmask <zijaham_link@hotmail.com>
2023-11-29 09:28:05 -0600

    Last Update to ApiRest

--- index.js
+++ index.js
@@ -36,29 +36,63 @@ async function readDocumentSamples(dirFile, typeFile){
 async function readSamples(){
     console.log('Creando registros...');
     // AUTOS SAMPLES
-    readDocumentSamples("/src/documents/sample_auto_Banorte.txt", "Automóviles-Flotilla");
-    readDocumentSamples("/src/documents/sample_auto_Chubb.txt", "Automóviles-Flotilla");
-    readDocumentSamples("/src/documents/sample_auto_Gnp.txt", "Automóviles-Flotilla");
-    readDocumentSamples("/src/documents/sample_auto_Primero.txt", "Automóviles-Flotilla");
-    readDocumentSamples("/src/documents/sample_auto_Qualitas.txt", "Automóviles-Flotilla");
+    readDocumentSamples("/src/documents/sample_auto_Banorte.txt", "Automóviles-Flotilla-Banorte");
+    readDocumentSamples("/src/documents/sample_auto_Banorte2.txt", "Automóviles-Flotilla-Banorte");
+    readDocumentSamples("/src/documents/sample_auto_Banorte3.txt", "Automóviles-Flotilla-Banorte");
+    readDocumentSamples("/src/documents/sample_auto_Chubb.txt", "Automóviles-Flotilla-Chubb");
+    readDocumentSamples("/src/documents/sample_auto_Chubb2.txt", "Automóviles-Flotilla-Chubb");
+    readDocumentSamples("/src/documents/sample_auto_Chubb3.txt", "Automóviles-Flotilla-Chubb");
+    readDocumentSamples("/src/documents/sample_auto_Gnp.txt", "Automóviles-Flotilla-Gnp");
+    readDocumentSamples("/src/documents/sample_auto_Gnp2.txt", "Automóviles-Flotilla-Gnp");
+    readDocumentSamples("/src/documents/sample_auto_Gnp3.txt", "Automóviles-Flotilla-Gnp");
+    readDocumentSamples("/src/documents/sample_auto_Primero.txt", "Automóviles-Flotilla-Primero");
+    readDocumentSamples("/src/documents/sample_auto_Primero2.txt", "Automóviles-Flotilla-Primero");
+    readDocumentSamples("/src/documents/sample_auto_Primero3.txt", "Automóviles-Flotilla-Primero");
+    readDocumentSamples("/src/documents/sample_auto_Qualitas.txt", "Automóviles-Flotilla-Qualitas");
+    readDocumentSamples("/src/documents/sample_auto_Qualitas2.txt", "Automóviles-Flotilla-Qualitas");
+    readDocumentSamples("/src/documents/sample_auto_Qualitas3.txt", "Automóviles-Flotilla-Qualitas");
     // DENTAL SAMPLES
-    readDocumentSamples("/src/documents/sample_dental_Dentalia.txt", "Dental");
-    readDocumentSamples("/src/documents/sample_dental_Dentegra.txt", "Dental");
+    readDocumentSamples("/src/documents/sample_dental_Dentalia.txt", "Dental-Dentalia");
+    readDocumentSamples("/src/documents/sample_dental_Dentalia2.txt", "Dental-Dentalia");
+    readDocumentSamples("/src/documents/sample_dental_Dentalia3.txt", "Dental-Dentalia");
+    readDocumentSamples("/src/documents/sample_dental_Dentegra.txt", "Dental-Dentegra");
+    readDocumentSamples("/src/documents/sample_dental_Dentegra2.txt", "Dental-Dentegra");
+    readDocumentSamples("/src/documents/sample_dental_Dentegra3.txt", "Dental-Dentegra");
     // GMM Samples
-    readDocumentSamples("/src/documents/sample_gmm_Atlas.txt", "GMM");
-    readDocumentSamples("/src/documents/sample_gmm_Gnp.txt", "GMM");
-    readDocumentSamples("/src/documents/sample_gmm_Axxa.txt", "GMM");
-    readDocumentSamples("/src/documents/sample_gmm_Metlife.txt", "GMM");
+    readDocumentSamples("/src/documents/sample_gmm_Atlas.txt", "GMM-Atlas");
+    readDocumentSamples("/src/documents/sample_gmm_Atlas2.txt", "GMM-Atlas");
+    readDocumentSamples("/src/documents/sample_gmm_Atlas3.txt", "GMM-Atlas");
+    readDocumentSamples("/src/documents/sample_gmm_Gnp.txt", "GMM-Gnp");
+    readDocumentSamples("/src/documents/sample_gmm_Gnp2.txt", "GMM-Gnp");
+    readDocumentSamples("/src/documents/sample_gmm_Gnp3.txt", "GMM-Gnp");
+    readDocumentSamples("/src/documents/sample_gmm_Axxa.txt", "GMM-Axxa");
+    readDocumentSamples("/src/documents/sample_gmm_Axxa2.txt", "GMM-Axxa");
+    readDocumentSamples("/src/documents/sample_gmm_Axxa3.txt", "GMM-Axxa");
+    readDocumentSamples("/src/documents/sample_gmm_Metlife.txt", "GMM-Metlife");
+    readDocumentSamples("/src/documents/sample_gmm_Metlife2.txt", "GMM-Metlife");
+    readDocumentSamples("/src/documents/sample_gmm_Metlife3.txt", "GMM-Metlife");
     // GMMenores Samples
-    readDocumentSamples("/src/documents/sample_gmmen_MediAccess.txt", "GMMenores");
-    readDocumentSamples("/src/documents/sample_gmmen_Mms.txt", "GMMenores");
+    readDocumentSamples("/src/documents/sample_gmmen_MediAccess.txt", "GMMenores-MediAccess");
+    readDocumentSamples("/src/documents/sample_gmmen_MediAccess2.txt", "GMMenores-MediAccess");
+    readDocumentSamples("/src/documents/sample_gmmen_MediAccess3.txt", "GMMenores-MediAccess");
+    readDocumentSamples("/src/documents/sample_gmmen_Mms.txt", "GMMenores-Mms");
+    readDocumentSamples("/src/documents/sample_gmmen_Mms2.txt", "GMMenores-Mms");
+    readDocumentSamples("/src/documents/sample_gmmen_Mms3.txt", "GMMenores-Mms");
     // Hogar Samples
-    readDocumentSamples("/src/documents/sample_hogar_Chubb.txt", "Hogar");
-    readDocumentSamples("/src/documents/sample_hogar_Zurich.txt", "Hogar");
+    readDocumentSamples("/src/documents/sample_hogar_Chubb.txt", "Hogar-Chubb");
+    readDocumentSamples("/src/documents/sample_hogar_Chubb2.txt", "Hogar-Chubb");
+    readDocumentSamples("/src/documents/sample_hogar_Chubb3.txt", "Hogar-Chubb");
+    readDocumentSamples("/src/documents/sample_hogar_Zurich.txt", "Hogar-Zurich");
+    readDocumentSamples("/src/documents/sample_hogar_Zurich2.txt", "Hogar-Zurich");
+    readDocumentSamples("/src/documents/sample_hogar_Zurich3.txt", "Hogar-Zurich");
     // Vida Samples
-    readDocumentSamples("/src/documents/sample_vida_Gnp.txt", "Vida");
+    readDocumentSamples("/src/documents/sample_vida_Gnp.txt", "Vida-Gnp");
+    readDocumentSamples("/src/documents/sample_vida_Gnp2.txt", "Vida-Gnp");
+    readDocumentSamples("/src/documents/sample_vida_Gnp3.txt", "Vida-Gnp");
     // Funerario Samples
-    readDocumentSamples("/src/documents/sample_funerario_Thona.txt", "Funerario");
+    readDocumentSamples("/src/documents/sample_funerario_Thona.txt", "Funerario-Thona");
+    readDocumentSamples("/src/documents/sample_funerario_Thona2.txt", "Funerario-Thona");
+    readDocumentSamples("/src/documents/sample_funerario_Thona3.txt", "Funerario-Thona");
     console.log("Read Sample files content ✅");
 }
 
@@ -95,6 +129,29 @@ async function fnLoadingClassification(){
     });
 }
 
+async function fnLoadClassifyFileFromJson(jsonName, dirFile){
+    natural.BayesClassifier.load(jsonName, null, function(err, classifier) {
+        classifyDocumentSamples(dirFile);
+    });
+}
+
+async function fnLoadGetClassifyFileFromJson(jsonName, dirFile){
+    natural.BayesClassifier.load(jsonName, null, function(err, classifier) {
+        fnGetClassificationsDocumment(dirFile);
+    });
+}
+
+async function fnLoadGetClassifyDataFromJson(jsonName, data){
+    natural.BayesClassifier.load(jsonName, null, function(err, classifier) {
+        console.log(classifier.classify(data));
+        return classifier.classify(data)
+      });
+}
+
+/* async function mainReadClassifyDoc(jsonName, data){
+    await fnLoadGetClassifyDataFromJson(jsonName, data);
+} */
+
 async function main(){
     await readSamples();
     await fnTrainingDataIA();
@@ -114,10 +171,24 @@ app.post("/extract-text", (req, res) => {
 });
 
 app.post("/classify-text", (req, res) => {
-    var texto = req.body.resultText;
-    console.log(texto);
-    res.render(__dirname + "/public/classify-text.html", {texto:texto});
+    
+   /*  let texto = fnLoadGetClassifyDataFromJson(req.body.resultText)
+    console.log(texto); */
+    /* let texto = mainReadClassifyDoc('clasificaciones.json',req.body.resultText)
+    console.log ('Variable text: ' + texto); */
+    let texto = classifier.classify(req.body.resultText)
+    console.log(texto)
+    //res.render(__dirname + "/public/classify-text.html", {texto:texto});}
+    //const r = "{result:'"+texto+"'}"
+    res.send("{result:'"+texto+"'}")
 });
+
+app.post("/file",(req,res) => {
+    
+    let texto = classifier.classify(req.body.resultText)
+    res.send("{result:'algo'}")
+})
+
 
 app.listen(app.get('port'), ()=>{
     console.log('App listening in port ' + app.get('port'))
