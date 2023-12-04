ed65a85f10eca6c6475bfdeca06aec7ea1b0252a
Deathmajorasmask <zijaham_link@hotmail.com>
2023-12-04 16:53:49 -0600

    NaturalControllerInit

--- src/controller/ocrfile.controller.js
+++ src/controller/ocrfile.controller.js
@@ -1,8 +1,7 @@
 const { ocrSpace } = require('ocr-space-api-wrapper');
 
 async function fnOcrExtractData(dirPathDoc){
-    const res1 = await ocrSpace( __basedir + "/resources/static/assets/uploads/" + dirPathDoc);
-    console.log("Entro aquiiiii")
+    let res1 = await ocrSpace( __basedir + "/resources/static/assets/uploads/" + dirPathDoc);
     console.log(res1)
     return res1
 }
